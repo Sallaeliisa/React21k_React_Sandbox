eb2ab14b6d20ce0470c9b93bd1f3babefd33b33c
Vuorikko Salla <s2100129@edu.bc.fi>
2021-03-25 13:12:28 +0200

    boxes box component

--- boxes/src/App.js
+++ boxes/src/App.js
@@ -1,10 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+const Box = (props) => {
+  return (
+    <div className="box">
+      <h2>Name: {props.name}</h2>
+      <p>Age: {props.age}</p>
+      <p>Title: {props.title}</p>
+    </div>
+  );
+};
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div>
+    <header>
+      <h1>Logo</h1>
+    </header>
+    <main>
+      <Box name="Salla Vuorikko" age="33" title="student" />
+      <Box name="Taimi from Kujala" age="6" title="the dog" />
+      <Box name="Jane Doan" age="50" title="doctor" />
+    </main>
+    <footer>
+      <p>copyright</p>
+    </footer>
     </div>
   );
 }
